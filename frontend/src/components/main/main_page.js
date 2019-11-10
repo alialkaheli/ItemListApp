f9dd54bb4099927c5ee5ea202778820fc6e79818
ali alkaheli <ali.alkaheli96@gmail.com>
2019-11-10 15:10:53 -0800

    main page

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>The List Item App</h1>
+                
+            </div>
+        );
+    }
+}
+
+export default MainPage;
